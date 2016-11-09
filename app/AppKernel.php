20998ac15c0fe49b0419f895c6d1638aa4132fe5
Mattia Clementi <mattia@qweb.eu>
2016-11-09 14:48:22 +0100

    Installato bundle per generazione sezione admin veloce

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new IlCleme\MigraDbBundle\IlClemeMigraDbBundle(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
